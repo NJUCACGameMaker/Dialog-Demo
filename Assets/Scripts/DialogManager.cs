675638a55a59e430df1020530bdb06cebbbf9b12
frankhjwx <frankhjwx@gmail.com>
2018-09-23 13:05:34 +0800

    更新文字动画

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -7,14 +7,17 @@ public class DialogManager : MonoBehaviour
 {
     private static DialogManager instance;
     public GameObject DialogPrefab;
-    public GameObject DialogBox;             // 对话框+立绘整体
-    public GameObject NameText;              // 显示角色姓名的文本框
-    public GameObject DialogText;            // 显示对话框文字的文本框
-    public GameObject Character;             // 立绘图像
-
-    private Dialog currentDialog;            // 当前对话
-    private int id;                          // temp 测试用
+    public GameObject DialogBox;               // 对话框+立绘整体
+    public double textSpeed = 0.04;            // 文字显示速度
+    
+    private Dialog currentDialog;              // 当前对话
+    private int id;                            // temp 测试用
     private DialogLoader loader;
+
+    private string tempDialog;                 // 逐字显示用
+    private bool dialogFlag;                   // 判断是否在逐字显示
+    private double timer;
+    private bool animationLock;                // 在播放特定动画的时候锁死交互
 
     public static DialogManager Instance
     {
@@ -35,19 +38,20 @@ public class DialogManager : MonoBehaviour
         DialogBox = Instantiate(DialogPrefab) as GameObject;
         currentDialog = loader.context[0];
         id = 0;
+        dialogFlag = false;
+        animationLock = false;
         displayDialog(currentDialog);
     }
 
     private void displayDialog(Dialog dialog)
     {
-        Text nameText = DialogBox.transform.Find("NameText").GetComponent<Text>();
-        nameText.text = dialog.characterName;
-        Text dialogText = DialogBox.transform.Find("DialogText").GetComponent<Text>();
-        dialogText.text = dialog.text;
         Image characterImage = DialogBox.transform.Find("Character").GetComponent<Image>();
         Sprite sp = Resources.Load(dialog.imagePath, typeof(Sprite)) as Sprite;
         Debug.Log(dialog.imagePath);
         characterImage.sprite = sp;
+        tempDialog = "";
+        timer = 0;
+        dialogFlag = true;
     }
 
     public void DestoryDiaLog()
@@ -75,18 +79,82 @@ public class DialogManager : MonoBehaviour
         initDialog();
     }
 
+    void Update()
+    {
+        if (tempDialog == currentDialog.text)
+        {
+            dialogFlag = false;
+        }
+        if (dialogFlag)
+        {
+            Text dialogText = DialogBox.transform.Find("DialogPanel").Find("DialogText").GetComponent<Text>();
+            if (timer > textSpeed)
+            {
+                timer = 0;
+                tempDialog = currentDialog.text.Substring(0, tempDialog.Length + 1);
+            }
+            dialogText.text = tempDialog;
+            timer += Time.deltaTime;
+        }
+    }
+
+    private IEnumerator nameAnimation(string name1, string name2)
+    {
+        GameObject replacedName = Instantiate(DialogBox.transform.Find("NamePanel").Find("NameText").gameObject) as GameObject;
+        replacedName.transform.position = DialogBox.transform.Find("NamePanel").Find("NameText").position - new Vector3(20f, 0, 0);
+        replacedName.GetComponent<Text>().text = name2;
+        Color c1 = DialogBox.transform.Find("NamePanel").Find("NameText").GetComponent<Text>().color;
+        Color c2 = replacedName.GetComponent<Text>().color;
+        c2 = new Color(c2.r, c2.g, c2.b, 0f);
+        replacedName.GetComponent<Text>().color = c2;
+        replacedName.transform.parent = DialogBox.transform.Find("NamePanel");
+
+        for (float t = 1f; t > 0; t -= 0.05f)
+        {
+            DialogBox.transform.Find("NamePanel").Find("NameText").transform.position += new Vector3(1f, 0, 0);
+            replacedName.transform.position += new Vector3(1f, 0, 0);
+            c1.a -= 0.05f;
+            c2.a += 0.05f;
+            DialogBox.transform.Find("NamePanel").Find("NameText").GetComponent<Text>().color = c1;
+            replacedName.GetComponent<Text>().color = c2;
+            yield return null;//下一帧继续执行for循环
+            yield return new WaitForSeconds(0.006f);//0.006秒后继续执行for循环
+        }
+        DialogBox.transform.Find("NamePanel").Find("NameText").transform.position -= new Vector3(20f, 0, 0);
+        DialogBox.transform.Find("NamePanel").Find("NameText").GetComponent<Text>().color = new Color(c1.r, c1.g, c1.b, 1.0f);
+        DialogBox.transform.Find("NamePanel").Find("NameText").GetComponent<Text>().text = name2;
+        Destroy(replacedName);
+        animationLock = false;
+    }
+
     private void setNextDialog()
     {
+        string name1 = currentDialog.characterName;
         id++;
         if (id >= loader.context.Count)
             id = 0;
         currentDialog = loader.context[id];
+        if (name1 != currentDialog.characterName)
+        {
+            animationLock = true;
+            StartCoroutine(nameAnimation(name1, currentDialog.characterName));
+        }
         displayDialog(currentDialog);
     }
 
     private void OnGUI()
     {
-        if (Event.current != null && Event.current.type == EventType.MouseDown)
-            setNextDialog();
+        if (!animationLock && Event.current != null && Event.current.type == EventType.MouseDown) {
+            // 如果当前文字已经全部出现，则进入下一句
+            // 否则将当前这句话直接显示出来
+            if (tempDialog == currentDialog.text) {
+                setNextDialog();
+            }
+            else { 
+                tempDialog = currentDialog.text;
+                Text dialogText = DialogBox.transform.Find("DialogPanel").Find("DialogText").GetComponent<Text>();
+                dialogText.text = tempDialog;
+            }
+        }
     }
 }
